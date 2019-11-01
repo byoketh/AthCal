e4cbfadaaf4f4ded6bbbe7a1226640c40a75c3ea
richie <rdcordrey@gmail.com>
2019-10-31 22:04:12 -0400

    Corrected variable assignment

--- scrape_html.py
+++ scrape_html.py
@@ -4,7 +4,7 @@ import csv
 
 URL = "https://www.chlathletics.org/g5-bin/client.cgi?cwellOnly=1&G5MID=052119071109056048054057085057079121108097120099075117068089100068089084043048101043098053076105052097056050047110098085078120075090073066076116116112089043067103066054066121048&G5statusflag=view&G5genie=661&G5button=12&vw_worksheet=4087&vw_type=mySchoolOnly&school_id=7"
 r = requests.get(URL)
-rawhtml = BeautifulSoup(r.content, 'html5lib')
+soup = BeautifulSoup(r.content, 'html5lib')
 quotes=[]
 
 table = soup.find('div', attrs = {'id':'container'})
