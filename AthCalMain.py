bc6305362df51cf9a007ae1475af382fb24f9ec1
richie <rdcordrey@gmail.com>
2019-11-12 09:05:52 -0500

    0.9.5 Logic Rework

--- AthCalMain.py
+++ AthCalMain.py
@@ -1,5 +1,5 @@
 try:
-    from confg import *
+    from config import *
     print("config file imported")
 except ModuleNotFoundError:
     print('\33[31m' + 'Err: config.py was not found' + '\033[0m')
@@ -10,13 +10,31 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from bs4 import BeautifulSoup
 # from datetime import datetime
 
+cell_sportURL = 0
+cell_sport = 0
+datecell = 34
+typecell = 35
+timecell = 36
+oppocell = 37
+locacell = 38
+name = list_sport[cell_sport]
+UIDnum = 0
+UIDnum_prefix = 1000
+
 # HTML request/parse
 print("Connecting...")
 try:
-    page = requests.get("https://www.chlathletics.org/g5-bin/client.cgi?cwellOnly=1&G5MID=052119071109056048054057085057079121108097120099075117068089100068089084043048101043098053076105052097056050047110098085078120075090073066076116116112089043067103066054066121048&G5statusflag=view&G5genie=661&G5button=12&vw_worksheet=4087&vw_type=mySchoolOnly&school_id=7")
+    page = requests.get(list_sportURL[cell_sportURL])
 except ConnectionRefusedError:
     print("Connection refused")
-page
+
+
+if str(page) == "<Response [200]>":
+    print("Connected")
+else:
+    print("Connection timed out")
+
+
 rawhtml = BeautifulSoup(page.content, 'html.parser')
 
 # OAuth 2.0 Setup
@@ -30,38 +48,25 @@ flow = InstalledAppFlow.from_client_secrets_file('client_secret_THS.json', scope
 credentials = pickle.load(open("token.pkl", "rb"))
 service = build("calendar", "v3", credentials=credentials)
 
-loopbool = True
-datecell = 34
-typecell = 35
-timecell = 36
-oppocell = 37
-locacell = 38
-UIDnum = 0
-
-def func_end_of_schedule():
-    print ("")
-
-def func_loop():
-    global loopbool, datecell, typecell, timecell, oppocell, locacell, UIDnum
-    datecell += 9
-    typecell += 9
-    timecell += 9
-    oppocell += 9
-    locacell += 9
-    UIDnum += 1
-    if loopbool == True:
-        func_new_event()
-
 #Define new_event function
 def func_new_event():
-    global loopbool, datecell, typecell, timecell, oppocell, locacell, UIDnum
-    global time, location, type, date
+    global datecell, timecell, locacell, typecell, oppocell, UIDnum, UIDnum_prefix, cell_sportURL, cell_sport, name
+    name = list_sport[cell_sport]
     date = rawhtml.find_all('td')[datecell].get_text()
     time = rawhtml.find_all('td')[timecell].get_text()
     if date == '':
-        print("Stop here")
-    elif time == r'TBD\xa0':
-        func_loop()
+        end_of_schedule = "End of %s Schedule" % (name)
+        print(end_of_schedule)
+        datecell = 34
+        typecell = 35
+        timecell = 36
+        oppocell = 37
+        locacell = 38
+        UIDnum = 0
+        UIDnum_prefix += 1000
+        cell_sportURL += 1
+        cell_sport += 1
+        name = list_sport[cell_sport]
     else:
         location = rawhtml.find_all('td')[locacell].get_text()
         type = rawhtml.find_all('td')[typecell].get_text()
@@ -100,54 +105,65 @@ def func_new_event():
         if time[1] == ':':
             time = '0' + time
         if time[:2] == 'TB':
-            func_loop()
-        try:
-            hour = int(time[:2])
-        except ValueError:
-            func_end_of_schedule()
-        meridian = time[5:]
-        #Special-case '12AM' -> 0, '12PM' -> 12 (not 24)
-        if (hour == 12):
-            hour = 0
-        if (meridian == 'PM'):
-            hour += 12
-        time = "%02d" % hour + time[2:8]
-        time = time[:-2]
-        #Put all time related variables together
-        finaldate = year + "-" + month + "-" + day + "T" + time + ":00.000-05:00"
-        starttime = finaldate
-        changehour = int(finaldate[11:-16])
-        changehour += 2
-        endtime = finaldate[:11] + str(changehour) + finaldate[13:]
-        #Format location
-        location = location[:-1]
-        #Format type
-        type = type[:-1]
-        #Create title
-        title = location + " " + type
-        #Define event iCalUID
-        UID = "UID" + str(UIDnum)
-        event = {
-          'summary': title,
-          'location': location,
-          'organizer': {
-            'email': 'thsathcal@gmail.com',
-            'displayName': 'CHLAthCal'
-          },
-          'start': {
-            'dateTime': starttime
-          },
-          'end': {
-            'dateTime': endtime
-          },
-          'iCalUID': UID
-        }
-        imported_event = service.events().import_(calendarId='primary', body=event).execute()
-        print ("Event imported ID: " + imported_event['id'])
-        func_loop()
+            datecell += 9
+            typecell += 9
+            timecell += 9
+            oppocell += 9
+            locacell += 9
+            print("Skipped event with TBA date")
+        else:
+            try:
+                hour = int(time[:2])
+            except ValueError:
+                func_end_of_schedule()
+            meridian = time[5:]
+            #Special-case '12AM' -> 0, '12PM' -> 12 (not 24)
+            if (hour == 12):
+                hour = 0
+            if (meridian == 'PM'):
+                hour += 12
+            time = "%02d" % hour + time[2:8]
+            time = time[:-2]
+            #Put all time related variables together
+            finaldate = year + "-" + month + "-" + day + "T" + time + ":00.000-05:00"
+            starttime = finaldate
+            changehour = int(finaldate[11:-16])
+            changehour += 2
+            endtime = finaldate[:11] + str(changehour) + finaldate[13:]
+            #Format location
+            location = location[:-1]
+            #Format type
+            type = type[:-1]
+            #Create title
+            title = name + " " + location + " " + type
+            #Define event iCalUID
+            UIDfull = UIDnum_prefix + UIDnum
+            UID = str(UIDfull)
+            event = {
+              'summary': title,
+              'location': location,
+              'organizer': {
+                'email': 'thsathcal@gmail.com',
+                'displayName': 'CHLAthCal'
+              },
+              'start': {
+                'dateTime': starttime
+              },
+              'end': {
+                'dateTime': endtime
+              },
+              'iCalUID': UID
+            }
+            imported_event = service.events().import_(calendarId='primary', body=event).execute()
+            print ("Event imported ID: " + imported_event['id'])
+            datecell += 9
+            typecell += 9
+            timecell += 9
+            oppocell += 9
+            locacell += 9
+            UIDnum += 1
 
-
-if loopbool == True:
+while True:
     func_new_event()
-else:
-    print("END")
+
+print("END")
